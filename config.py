f617dbc51fef5904cf8c4b0d620f624d68ef1f66
Yashraj Jagdale <159432802+yashraj022381@users.noreply.github.com>
2026-05-12 11:44:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,6 @@ class Config:
     # postgresql://username:password@host:port/dbname
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
         "postgresql://neondb_owner:npg_J4XPGqNfC6Hl@ep-icy-shadow-ap159j2b-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
      # Disable modification tracking (saves memory)
